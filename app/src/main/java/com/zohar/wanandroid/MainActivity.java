9d71ee76fde88e502b314e280f84e0ff8ba51856
ZoharAndroid <1048132071@qq.com>
2019-08-07 23:19:08 +0800

    侧滑栏和底部导航栏完成

--- app/src/main/java/com/zohar/wanandroid/MainActivity.java
+++ app/src/main/java/com/zohar/wanandroid/MainActivity.java
@@ -1,20 +1,78 @@
 package com.zohar.wanandroid;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.zohar.wanandroid.utils.KeyboardUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mEditText;
-
+    private Toolbar mToolbar;
+    private DrawerLayout mMainDrawerLayout;
+    private NavigationView mNavigationView;
+    private FloatingActionButton mFloatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEditText = findViewById(R.id.edit_text);
+        mToolbar = findViewById(R.id.toolbar);
+        mMainDrawerLayout = findViewById(R.id.main_drawer_layout);
+        mNavigationView = findViewById(R.id.navigation_container);
+        mFloatingActionButton = findViewById(R.id.floating_action_button);
+        
+        setSupportActionBar(mToolbar);
+        // 设置侧滑菜单
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.icon_menu);
+        }
+        mNavigationView.setCheckedItem(R.id.collections);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mMainDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
+        // 滑动点按钮
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.men_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search_tool_bar:
+                Toast.makeText(MainActivity.this, "搜索", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                mMainDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return true;
     }
 }
