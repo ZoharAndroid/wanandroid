c17a27fe6d0e1efcc2bd04a80f133865f5ddc8e8
ZoharAndroid <1048132071@qq.com>
2019-08-25 22:11:04 +0800

    add loadmore

--- app/src/main/java/com/zohar/wanandroid/adapter/HomeArticleAdapter.java
+++ app/src/main/java/com/zohar/wanandroid/adapter/HomeArticleAdapter.java
@@ -2,6 +2,7 @@ package com.zohar.wanandroid.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,7 +14,6 @@ import android.widget.TextView;
 import com.zohar.wanandroid.ArticleDetailActivity;
 import com.zohar.wanandroid.R;
 import com.zohar.wanandroid.bean.home.Datas;
-import com.zohar.wanandroid.utils.LogUtils;
 
 import java.util.List;
 
@@ -21,10 +21,16 @@ import java.util.List;
  * Created by zohar on 2019/8/8 17:08
  * Describe:
  */
-public class HomeArticleAdapter extends RecyclerView.Adapter<HomeArticleAdapter.ArticleViewHolder> {
+public class HomeArticleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private List<Datas> articles;
     private Context mContext;
+
+    // 正常内容
+    private final static int TYPE_CONTENT = 0;
+    // 下来刷新
+    private final static int TYPE_FOOTER = 1;
+
 
     public HomeArticleAdapter(Context context, List<Datas> articles) {
         mContext = context;
@@ -32,57 +38,86 @@ public class HomeArticleAdapter extends RecyclerView.Adapter<HomeArticleAdapter.
         // LogUtils.d("传递过来的数据为：" + articles.toString() );
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == articles.size()){
+            return TYPE_FOOTER;
+        }
+        return TYPE_CONTENT;
+    }
+
     @NonNull
     @Override
-    public ArticleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.item_home_article, viewGroup, false);
-        final ArticleViewHolder holder = new ArticleViewHolder(view);
-        // 设置点击事件
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = holder.getAdapterPosition(); // 获取当前点击的位置
-                Datas article = articles.get(position);
-                // 跳转到内容详情页面
-                String articleLink = article.getLink();
-                Intent intent = new Intent(mContext, ArticleDetailActivity.class);
-                intent.putExtra(ArticleDetailActivity.ARTICLE_FROM_HOME, articleLink);
-                mContext.startActivity(intent);
-            }
-        });
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        if (viewType == TYPE_CONTENT) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_home_article, viewGroup, false);
+            final ArticleViewHolder holder = new ArticleViewHolder(view);
+            // 设置点击事件
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = holder.getAdapterPosition(); // 获取当前点击的位置
+                    Datas article = articles.get(position);
+                    // 跳转到内容详情页面
+                    String articleLink = article.getLink();
+                    Intent intent = new Intent(mContext, ArticleDetailActivity.class);
+                    intent.putExtra(ArticleDetailActivity.ARTICLE_FROM_HOME, articleLink);
+                    mContext.startActivity(intent);
+                }
+            });
+            return holder;
+        }else{
+            // footer
+            View view = LayoutInflater.from(mContext).inflate(R.layout.item_home_article_footer, viewGroup, false);
+            return new FooterViewHolder(view);
+        }
 
 
-        return holder;
     }
 
     @Override
-    public void onBindViewHolder( ArticleViewHolder articleViewHolder, int position) {
-        // viewholder绑定的数据
-        Datas article = articles.get(position);
-        articleViewHolder.title.setText(article.getTitle());
-        articleViewHolder.author.setText(article.getAuthor());
-        articleViewHolder.date.setText(article.getNiceDate());
-        articleViewHolder.chapterName.setText(article.getChapterName());
-        articleViewHolder.superChapterName.setText(article.getSuperChapterName());
-        //LogUtils.d(article.toString());
-        if (article.isFresh()){
-            articleViewHolder.refresh.setVisibility(View.VISIBLE);
-        }
-        // 处理tag
-        if (article.getTags().size() != 0){
-            articleViewHolder.tag.setVisibility(View.VISIBLE);
-            articleViewHolder.tag.setText(article.getTags().get(0).getName());
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        if (getItemViewType(position) == TYPE_FOOTER){
+            // 上拉加载
+
+        }else {
+            // 正常内容
+            // viewholder绑定的数据
+            ArticleViewHolder articleViewHolder = (ArticleViewHolder) viewHolder;
+            Datas article = articles.get(position);
+            articleViewHolder.title.setText(article.getTitle());
+            articleViewHolder.author.setText(article.getAuthor());
+            articleViewHolder.date.setText(article.getNiceDate());
+            articleViewHolder.chapterName.setText(article.getChapterName());
+            articleViewHolder.superChapterName.setText(article.getSuperChapterName());
+            //LogUtils.d(article.toString());
+            if (article.isFresh()) {
+                articleViewHolder.refresh.setVisibility(View.VISIBLE);
+            }
+            // 处理tag
+            if (article.getTags().size() != 0) {
+                articleViewHolder.tag.setVisibility(View.VISIBLE);
+                articleViewHolder.tag.setText(article.getTags().get(0).getName());
+            }
         }
 
     }
 
     @Override
     public int getItemCount() {
-        return articles.size();
+        return articles.size() + 1;
     }
 
 
-    static class ArticleViewHolder extends RecyclerView.ViewHolder{
+    class FooterViewHolder extends RecyclerView.ViewHolder{
+
+        public FooterViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    static class ArticleViewHolder extends RecyclerView.ViewHolder {
 
         View itemView;
         TextView title;
@@ -94,7 +129,7 @@ public class HomeArticleAdapter extends RecyclerView.Adapter<HomeArticleAdapter.
         TextView refresh;
         TextView tag;
 
-        public ArticleViewHolder( View itemView) {
+        public ArticleViewHolder(View itemView) {
             super(itemView);
             this.itemView = itemView;
             title = itemView.findViewById(R.id.item_title);
